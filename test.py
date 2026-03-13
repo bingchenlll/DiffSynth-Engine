f2155631c41e6bfc67dbc882f266c4c68e9ec496
bingchenlll <bingchen@character.ai>
2026-03-13 20:11:11 +0000

    minor

--- test.py
+++ test.py
@@ -120,12 +120,12 @@ new_col.save(new_im_path)
         
 
 """
-org no compile: 2.01 it/s
-org no comp with fa3: 2.35 it/s
-org compile:    2.78 it/s
-org compile with fa3:  3.06 it/s
-cuda kernels no compile: 2.17 it/s
-cuda kernels: 2.79 it/s
+org no compile:                2.01 it/s
+org no comp with fa3:          2.35 it/s
+org compile no fa3:            2.78 it/s
+org compile with fa3:          3.06 it/s
+cuda kernels no compile:       2.17 it/s
+cuda kernels compile:          2.79 it/s
 cuda kernels compile with fa3: 3.06 it/s
 
 """
